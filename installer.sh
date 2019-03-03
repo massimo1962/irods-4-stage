a79fa836514c4b91ddcdd504ef3eddf2ddb439db
Massimo Fares <massimo.fares@ingv.it>
2019-03-03 01:54:31 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ service postgresql start
 # prep ICAT
 # 
 cd /var/lib/postgresql
-sudo -u postgres psql -c "CREATE USER irods WITH PASSWORD 'irods' " 
+sudo -u postgres psql -c "CREATE USER irods WITH PASSWORD 'xoxoxo' " 
 sudo -u postgres psql -c 'CREATE DATABASE "ICAT" ' 
 sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE "ICAT" TO irods ' 
 
